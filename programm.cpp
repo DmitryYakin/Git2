75331678f32ed39746d21a9613b89bec1eddb5ce
NDK619 <KuvshinovND@yandex.ru>
2021-12-13 19:24:30 +0300

    Добавил умножение и деление в основной файл программы

--- programm.cpp
+++ programm.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 int main()
 {
-    double a,b,i;
+double a,b;
+int i;
 cout<<"Enter operation's number which u wanna do"<<endl;
 cout<<"1 - Sum, 2- Minus, 3 - Umnozhenie, 4 - delenie"<<endl;
 cin>>i;
@@ -12,15 +13,16 @@ cin>>a>>b;
 switch (i){
 case 1: cout<< Plus(a,b) <<endl;
 break;
-case 2:  cout<<Minus(a,b )<<endl;
+case 2:  cout<<Minus(a,b)<<endl;
 break;
-case 3:  cout<<"rabotaem nad etim" <<endl;
+case 3:  cout<<Mul(a,b) <<endl;
 break;
-case 4: cout<<"rabotaem nad etim" <<endl;
+case 4: cout<<Div(a,b) <<endl;
 break;
 default: cout<<"Sorry but u enter wrong symbol"<<endl;
 break;
-}
+};
+
     system("pause");
 return 0;
 }
