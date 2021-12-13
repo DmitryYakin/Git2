f8e8ab4b914b99c38a4a41353c26ec8b861901d4
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2021-12-13 18:01:26 +0300

    Update programm.cpp

--- programm.cpp
+++ programm.cpp
@@ -3,8 +3,23 @@
 using namespace std;
 int main()
 {
+    double a,b,i;
+cout<<"Enter operation's number which u wanna do"<<endl;
+cout<<"1 - Sum, 2- Minus, 3 - Umnozhenie, 4 - delenie"<<endl;
+cin>>i;
+cout<< "enter 2 numbers"<<endl;
+cin>>a>>b;
+switch (i){
+case 1: cout<<"rabotaem nad etim" <<endl;
+break;
+case 2:  cout<<"rabotaem nad etim" <<endl;
+break;
+case 3:  cout<<"rabotaem nad etim" <<endl;
+break;
+case 4: cout<<"rabotaem nad etim" <<endl;
+break;
 
-    
+}
     system("pause");
 return 0;
 }
